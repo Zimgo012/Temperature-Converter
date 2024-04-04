e66a7799585eb51e04e44b2f142f538482627cb3
John Rycca <126682794+Zimgo012@users.noreply.github.com>
2024-04-04 12:10:53 -0400

    Created Main class for the project

---
+++ Driver.java
@@ -0,0 +1,13 @@
+/* Program by: John Rycca Belcina
+   Date Started : April 04, 2024
+ */
+
+
+/* Main class for Temperature Converter Project */
+public class Driver {
+
+    public static void main(String[] args) {
+
+
+    }
+}
