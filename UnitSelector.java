cec51fc944ee4263f4e685cee6dfe2f8720f6267
John Rycca <126682794+Zimgo012@users.noreply.github.com>
2024-04-09 19:30:21 -0400

    Added loop for each nested case in UnitSelector.java that prompts the user to Continue converting or not.

--- UnitSelector.java
+++ UnitSelector.java
@@ -2,7 +2,6 @@
 It uses switch-case statements to handle different unit combinations.
  */
 
-
 public class UnitSelector {
 
     UnitConvertor unitConvertor = new UnitConvertor();
@@ -12,19 +11,36 @@ public class UnitSelector {
 
 
     public  void selector (int baseUnit, int derivedUnit ){
+                String respond; // store respond
 
         switch (baseUnit) {
             case 1:
                 switch (derivedUnit) {
                     case 2: // converts Celsius to Fahrenheit
-                        System.out.println("Celsius to Fahrenheit");
-                        unitConvertor.setCelsius(User.inputUnit("Please input number of Celsius: \n"));
-                        System.out.printf("Fahrenheit: %.2f", unitConvertor.calculateCToF()); //method
+                        do {
+
+
+                            System.out.println("Celsius to Fahrenheit");
+                            unitConvertor.setCelsius(User.inputUnit("Please input number of Celsius: \n"));
+                            System.out.printf("Fahrenheit: %.2f %n", unitConvertor.calculateCToF()); //method
+
+                            respond = User.inputRespond("Convert again? YES / NO\n");
+
+
+                        } while (respond.equalsIgnoreCase("YES"));
                         break;
+
                     case 3: //converts Celsius to Kelvin
-                        System.out.println("Celsius to Kelvin");
-                        unitConvertor.setCelsius(User.inputUnit("Please input number of Celsius: \n"));
-                        System.out.printf("Kelvin: %.2f", unitConvertor.calculateCToK()); //method
+
+                        do {
+
+                            System.out.println("Celsius to Kelvin");
+                            unitConvertor.setCelsius(User.inputUnit("Please input number of Celsius: \n"));
+                            System.out.printf("Kelvin: %.2f %n", unitConvertor.calculateCToK()); //method
+
+                            respond = User.inputRespond("Convert again? YES / NO\n");
+
+                        } while (respond.equalsIgnoreCase("YES"));
                         break;
 
                 }
@@ -32,14 +48,24 @@ public class UnitSelector {
             case 2:
                 switch (derivedUnit) {
                 case 1: // converts Fahrenheit to Celsius
-                    System.out.println("Fahrenheit to Celsius");
-                    unitConvertor.setFahrenheit(User.inputUnit("Please input number of Fahrenheit: \n"));
-                    System.out.printf("Celsius: %.2f", unitConvertor.calculateFToC()); //method
+                    do {
+                        System.out.println("Fahrenheit to Celsius");
+                        unitConvertor.setFahrenheit(User.inputUnit("Please input number of Fahrenheit: \n"));
+                        System.out.printf("Celsius: %.2f %n", unitConvertor.calculateFToC()); //method
+
+                        respond = User.inputRespond("Convert again? YES / NO\n");
+
+                    } while (respond.equalsIgnoreCase("YES"));
                     break;
                 case 3: // converts Fahrenheit to Kelvin
-                    System.out.println("Fahrenheit to Kelvin");
-                    unitConvertor.setFahrenheit(User.inputUnit("Please input number of Fahrenheit: \n"));
-                    System.out.printf("Kelvin: %.2f", unitConvertor.calculateFToK()); //method
+                    do {
+                        System.out.println("Fahrenheit to Kelvin");
+                        unitConvertor.setFahrenheit(User.inputUnit("Please input number of Fahrenheit: \n"));
+                        System.out.printf("Kelvin: %.2f %n", unitConvertor.calculateFToK()); //method
+
+                        respond = User.inputRespond("Convert again? YES / NO\n");
+
+                    } while (respond.equalsIgnoreCase("YES"));
                     break;
 
             }
@@ -47,14 +73,25 @@ public class UnitSelector {
             case 3:
                 switch (derivedUnit){
                     case 1: // converts Kelvin to Celsius
-                        System.out.println("Kelvin to Celsius");
-                        unitConvertor.setKelvin(User.inputUnit("Please input number of Kelvin: \n"));
-                        System.out.printf("Celsius: %.2f", unitConvertor.calculateKToC()); //method
+                        do {
+                            System.out.println("Kelvin to Celsius");
+                            unitConvertor.setKelvin(User.inputUnit("Please input number of Kelvin: \n"));
+                            System.out.printf("Celsius: %.2f %n", unitConvertor.calculateKToC()); //method
+
+                            respond = User.inputRespond("Convert again? YES / NO\n");
+
+                        } while (respond.equalsIgnoreCase("YES"));
                         break;
                     case 2: // convert Kelvin to Fahrenheit
-                        System.out.println("Kelvin to Fahrenheit");
-                        unitConvertor.setKelvin(User.inputUnit("Please input number of Kelvin: \n"));
-                        System.out.printf("Fahrenheit: %.2f", unitConvertor.calculateKToF()); //method
+                        do {
+                            System.out.println("Kelvin to Fahrenheit");
+                            unitConvertor.setKelvin(User.inputUnit("Please input number of Kelvin: \n"));
+                            System.out.printf("Fahrenheit: %.2f %n", unitConvertor.calculateKToF()); //method
+
+                            respond = User.inputRespond("Convert again? YES / NO\n");
+
+                        } while (respond.equalsIgnoreCase("YES"));
+                        break;
                 }
 
         }
