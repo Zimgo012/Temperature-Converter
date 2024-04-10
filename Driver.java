0b8338d1770b70cff0d1fd03afe5170d4e795a97
John Rycca <126682794+Zimgo012@users.noreply.github.com>
2024-04-10 09:38:22 -0400

    * Added a feature that ask the user if he wants to perform another conversion and if he responds "Yes" it will loop back from the start. * Updated string output statement for clarity.

--- Driver.java
+++ Driver.java
@@ -11,29 +11,37 @@ public class Driver {
         User user1 = new User();
         UnitSelector unitSelector = new UnitSelector();
 
-        System.out.println("Welcome to Temperature Converter");
+        String respond;
 
+        System.out.println("Temperature Converter");
+        System.out.println("=====================");
+    do {
         //Let user choose from the choices for the base unit.
         user1.setChoice1(User.inputFirstInteger("""
-                            Select unit to convert from:
-                            
-                            Press [1] for Celsius
-                            Press [2] for Fahrenheit \s
-                            Press [3] for Kelvin    \n"""));
+                Please choose the unit you wish to convert from:
+                                            
+                Press [1] for Celsius
+                Press [2] for Fahrenheit \s
+                Press [3] for Kelvin    \n"""));
 
         //Let user choose from the choices for the derive unit.
-       user1.setChoice2(User.inputSecondInteger("""
-                            Convert To:
-                            
-                            Press [1] for Celsius
-                            Press [2] for Fahrenheit \s
-                            Press [3] for Kelvin   \n"""));
+        user1.setChoice2(User.inputSecondInteger("""
+                Choose the unit you want to convert to:
+                                            
+                Press [1] for Celsius
+                Press [2] for Fahrenheit \s
+                Press [3] for Kelvin   \n"""));
 
 
         unitSelector.selector(user1.getChoice1(), user1.getChoice2());
 
+        respond = User.inputRespond("Would you like to perform another conversion?" +
+                                    " Please respond with either \"YES\" or \"NO\".\n");
 
+    } while(respond.equalsIgnoreCase("YES"));
 
+        System.out.println("Thank you for using this application!");
+        System.out.println("Program by: John Rycca Belcina");
 
 
     }
